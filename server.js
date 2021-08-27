2c509ac876f698ca3edce323c73d661b73ca561a
Rustambek Mamadaliyev <80699038+Rustambek-Mamadaliyev0301@users.noreply.github.com>
2021-08-27 15:11:02 +0500

    Initial server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express")
 const routes = require("./routes/routes") 
 const app = express()
 
- app.listen(process.env.PORT || 80); 
+ app.listen(process.env.PORT || 8000 ); 
 
  app.use(express.json())
  app.use(
